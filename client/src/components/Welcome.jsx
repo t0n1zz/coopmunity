86bd50e34b6e400e559a76b438dcf4885f31e347
Paman Beruang <tony762843@yahoo.co.id>
2023-05-01 17:00:00 +0700

    fix mobile

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,10 +5,14 @@ const Welcome = () => {
   return (
     <Link to="/faq">
       <div className="flex flex-col items-center justify-center bg-blue-500 h-96 rounded-lg mb-5">
-        <h1 className="text-4xl font-bold text-white mb-4">Welcome to COOPMUNITY</h1>
-        <p className="text-lg text-white">A Forum for Credit Union Members, Stafs and Supporters</p>
+        <h1 className="font-lato font-bold w-full text-white text-xl text-center">
+          Welcome to COOPMUNITY
+        </h1>
+        <p className="text-lg text-white text-center">
+          A Forum for Credit Union Members, Stafs and Supporters
+        </p>
         <hr className="w-16 h-1 bg-white my-4" />
-        <p className="text-md text-white">Click to learn more</p>
+        <p className="text-md text-white text-center">Click to learn more</p>
       </div>
     </Link>
   );
